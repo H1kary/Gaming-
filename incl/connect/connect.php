4b3c5199c310df74757b62b3b716bd53dcd4b03f
H1kary <babylya03@yandex.ru>
2025-02-09 21:05:30 +0300

    created

---
+++ incl/connect/connect.php
@@ -0,0 +1,9 @@
+<?php
+$localhost = "localhost";
+$dbname = "gamingplus";
+$username = "root";
+$password = "";
+
+
+$connect = new PDO("mysql:host=$localhost;dbname=$dbname",$username,$password);
+
